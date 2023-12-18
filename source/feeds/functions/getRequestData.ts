9e35688a4f2f803bafa9d90f4f5f98c0a0e00d73
Pablo Amomo <pabloamomo@gmail.com>
2023-12-18 08:56:10 +0100

    first commit

--- source/feeds/functions/getRequestData.ts
+++ source/feeds/functions/getRequestData.ts
@@ -2,11 +2,12 @@ import { Request } from 'express';
 import { RequestData } from '../models/RequestData';
 
 const getRequestData = (req: Request): RequestData => {
+  const body = req.body || {};
   const id: string = req.params.id;
   const count = req.query.count ? parseInt(req.query.count as string) : 10;
   const user = req.header('x-user') ? (req.header('x-user') as string) : '';
   const filter = req.query.filter ? (req.query.filter as string).split(',') : [];
-  const feedsId = req.query.feedsId ? (req.query.feedsId as string).split(',') : [];
+  const feedsId = body?.feedsId || req.query.feedsId ? (req.query.feedsId as string).split(',') : [];
   const back = req.query.back ? parseInt(req.query.back as string) : 0;
   return { count, user, filter, back, id: Number.isNaN(parseInt(id)) ? NaN : parseInt(id), feedsId };
 };
