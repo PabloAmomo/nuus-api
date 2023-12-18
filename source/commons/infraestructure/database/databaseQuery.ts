71f11f590564c148c8e08c54e09f826668e4c2ea
Pablo amomo <pabloamomo@gmail.com>
2023-12-18 16:14:38 +0100

    .

--- source/commons/infraestructure/database/databaseQuery.ts
+++ source/commons/infraestructure/database/databaseQuery.ts
@@ -3,7 +3,6 @@ import mysql from 'mysql';
 
 /** Query the database */
 async function databaseQuery(ConnectionConfig: mysql.ConnectionConfig , sql: string, values: any[]): Promise<any> {
-  // 'SELECT * FROM `books` WHERE `author` = ?', ['David']
   const promise = new Promise((resolve, reject) => {
     const connection = databaseConnection(ConnectionConfig);
     connection.on('error', (error) => reject(error));
